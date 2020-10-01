b38297f3ffc8766279f7e454888f233eb4b041b1
cedricplouvier <cedric.plouvier@student.uantwerpen.be>
2020-10-01 16:56:39 +0200

    "DJInterface html"

--- src/main/java/build/webinterface/Controller/DJController.java
+++ src/main/java/build/webinterface/Controller/DJController.java
@@ -1,16 +1,24 @@
 package build.webinterface.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class DJController {
-    public void addSongs() {
-        List<String> songs = new ArrayList<String>();
+
+    @RequestMapping(value={"/","/home"})
+    public String addSongs() {
+        /*List<String> songs = new ArrayList<String>();
         songs.add("Song1");
         songs.add("Song2");
-        songs.add("Song3");
+        songs.add("Song3");*/
+
+        return "DJInterface";
     }
+
+
 }
