4fd1f1b6c8a7cd1813bfa453a69081bd95aea108
cedricplouvier <cedric.plouvier@student.uantwerpen.be>
2020-10-01 17:56:46 +0200

    "list songs"

--- src/main/java/build/webinterface/Controller/DJController.java
+++ src/main/java/build/webinterface/Controller/DJController.java
@@ -1,7 +1,9 @@
 package build.webinterface.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
@@ -10,14 +12,26 @@ import java.util.List;
 @Controller
 public class DJController {
 
-    @RequestMapping(value={"/","/home"})
-    public String addSongs() {
-        /*List<String> songs = new ArrayList<String>();
-        songs.add("Song1");
-        songs.add("Song2");
-        songs.add("Song3");*/
+    @RequestMapping(value={"/","/home"}, method = RequestMethod.GET)
+    public String addSongs(final ModelMap model) {
+        List<String> songs = new ArrayList<String>();
+
+        String a="No song";
+
+        songs.add("SongA");
+        songs.add("SongB");
+        songs.add("SongC");
+        model.addAttribute("songs", songs);
+        model.addAttribute("selectedSong", a);
 
         return "DJInterface";
+    }
+
+    @RequestMapping(value="/home/submit")
+    public String submit(final ModelMap model, String selectedSong){
+        String test = (String) model.getAttribute(selectedSong);
+        System.out.println(test);
+        return "redirect:/home";
     }
 
 
