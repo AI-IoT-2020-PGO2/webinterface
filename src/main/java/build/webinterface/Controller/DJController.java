b5f9d9b98ed0db54cfb252cc340b8deaee09ce7e
cedricplouvier <cedric.plouvier@student.uantwerpen.be>
2020-10-04 19:07:21 +0200

    Selected song to controller

--- src/main/java/build/webinterface/Controller/DJController.java
+++ src/main/java/build/webinterface/Controller/DJController.java
@@ -1,13 +1,8 @@
 package build.webinterface.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,24 +10,25 @@ import java.util.List;
 @Controller
 public class DJController {
 
-    @RequestMapping(value={"/","/home"}, method = RequestMethod.GET)
+    @RequestMapping(value={"/","/home"})
     public String addSongs(final ModelMap model) {
         List<String> songs = new ArrayList<>();
-
-        String selectedSong="No song";
 
         songs.add("SongA");
         songs.add("SongB");
         songs.add("SongC");
         model.addAttribute("songs", songs);
-        model.addAttribute("selectedSong", selectedSong);
+        //model.addAttribute("selectedSong");
+        //System.out.println(model.getAttribute("selectedSong"));
 
         return "DJInterface";
     }
 
-    @RequestMapping(value="/home/submit", method = RequestMethod.POST)
-    public String submit(@ModelAttribute("selectedSong") String selectedSong){
-        System.out.println(selectedSong);
+    @RequestMapping(value="/home/submit")
+    public String submit(@ModelAttribute("selectedSong") String selectedSong, final ModelMap model){
+        System.out.println("submitted");
+        System.out.println(model.getAttribute("selectedSong"));
+        System.out.println("____________");
         return "redirect:/home";
     }
 }
